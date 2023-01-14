28e53ca44f46a22611d32eef42f5adb1377ec381
Amirhanna2001 <amirhanna111200@gmail.com>
2023-01-14 12:01:54 +0200

    Movie POST end point

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace MoviesAPI.Controllers
 {
@@ -7,5 +8,36 @@ namespace MoviesAPI.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm]CreateMovieDto dto)
+        {
+            using MemoryStream dataStream = new ();
+
+            await dto.Poster.CopyToAsync(dataStream);
+
+            Movie movie = new()
+            {
+                Title = dto.Title,
+                Year = dto.Year,
+                Rate = dto.Rate,
+                StoreLine = dto.StoreLine,
+                GenreId = dto.GenreId,
+                Poster = dataStream.ToArray()
+                
+            };
+
+            await _context.Movies.AddAsync(movie);
+
+            _context.SaveChanges();
+
+            return Ok(movie);
+        }
     }
 }
---
+++ MoviesAPI/Dtos/CreateMovieDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MoviesAPI.Dtos
+{
+    public class CreateMovieDto
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public Double Rate { get; set; }
+        public string StoreLine { get; set; }
+        public IFormFile Poster { get; set; }
+        public byte GenreId { get; set; }
+    }
+}
