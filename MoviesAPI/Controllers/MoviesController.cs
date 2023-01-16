930e8f2e43748961e423b18fc24db31fc83701fe
Amirhanna2001 <amirhanna111200@gmail.com>
2023-01-16 11:56:40 +0200

    GET end Point for all movies

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -5,18 +5,39 @@ using System;
 
 namespace MoviesAPI.Controllers
 {
+    //get post put delete
     [Route("api/[controller]")]
     [ApiController]
     public class MoviesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
         private long maxAllowedPosterSize = 1_048_576;
-        private List<string> allowedPosterExtentions = new() { "jpg", "png" };
+        private List<string> allowedPosterExtentions = new() { ".jpg", ".png" };
 
         public MoviesController(ApplicationDbContext context)
         {
             _context = context;
         }
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            return Ok(await _context.Movies
+                .OrderByDescending(m => m.Rate)
+                .Include(m => m.Genre)
+                .Select(m => new MovieDetailsDto
+                {
+                    Id = m.Id,
+                    GenreId = m.Genre.Id,
+                    GenreName = m.Genre.Name,
+                    Poster = m.Poster,
+                    Rate = m.Rate,
+                    StoreLine = m.StoreLine,
+                    Title = m.Title,
+                    Year = m.Year,
+                })
+                .ToListAsync());
+        }
+
 
         [HttpPost]
         public async Task<IActionResult> Create([FromForm]CreateMovieDto dto)
@@ -25,11 +46,14 @@ namespace MoviesAPI.Controllers
             //handle extenstions 
             if (dto.Poster.Length > maxAllowedPosterSize)
                 return BadRequest("Max allowed size is 1MB !");
-            
-            if (!allowedPosterExtentions.Contains(Path.GetExtension(dto.Poster.FileName.ToLower())))
-                return BadRequest("Only allowed extentions is PNG and JPG !");  
-            
-            if (await _context.Genres.AnyAsync(g=>g.Id == dto.GenreId))
+
+            if (!allowedPosterExtentions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+                return BadRequest("Only allowed extentions is PNG and JPG !");
+
+            //if (!allowedPosterExtentions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+            //    return BadRequest("Only .png and .jpg images are allowed!");
+
+            if (!await _context.Genres.AnyAsync(g=>g.Id == dto.GenreId))
                 return BadRequest("The Genre Id You enterd is not found");
 
 
